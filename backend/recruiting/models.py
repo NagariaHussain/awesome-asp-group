cd36f487cfa136473dd6e552aa5122c9816c7362
Hussain Nagaria <hussainbhaitech@gmail.com>
2022-01-13 18:18:43 +0530

    feat: Job Posting model

--- backend/recruiting/models.py
+++ backend/recruiting/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class JobPosting(models.Model):
+    job_title = models.CharField(max_length=255)
+    description = models.TextField()
+    is_published = models.BooleanField(default=False)
+    location = models.CharField(max_length=255)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.job_title} : {self.location}"
