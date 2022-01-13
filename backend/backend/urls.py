fffd4170c03cffb43a5948d91d5f294d94222500
Hussain Nagaria <hussainbhaitech@gmail.com>
2022-01-13 14:53:08 +0530

    style: isort

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -13,11 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import path, re_path
 from django.contrib import admin
-from django.views.generic import TemplateView
+from django.urls import path, re_path
 from django.views.decorators.cache import never_cache
-
+from django.views.generic import TemplateView
 
 vue_router = never_cache(TemplateView.as_view(template_name="index.html"))
 
