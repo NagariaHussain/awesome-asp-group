2a40fa69eb56537c806ca719e39713b6e48801ac
Hussain Nagaria <hussainbhaitech@gmail.com>
2022-01-13 18:22:30 +0530

    feat: Setup proxying API in dev setup

---
+++ frontend/proxyOptions.js
@@ -0,0 +1,12 @@
+const webserver_port = 8000;
+
+module.exports = {
+	"^/(api|assets|files|ws)": {
+		target: `http://localhost:${webserver_port}`,
+		ws: true,
+		router: function (req) {
+			const site_name = req.headers.host.split(":")[0];
+			return `http://${site_name}:${webserver_port}`;
+		},
+	},
+};
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,14 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import proxyOptions from "./proxyOptions";
 
 export default defineConfig({
 	build: {
 		outDir: "../backend/dist",
 	},
 	plugins: [vue()],
+	server: {
+		port: 3030,
+		proxy: proxyOptions,
+	},
 });
