5910b35d1142b7d1bf6856c61caf45d74390e103
Hussain Nagaria <hussainbhaitech@gmail.com>
2022-01-13 14:47:28 +0530

    style: Black

--- backend/recruiting/apps.py
+++ backend/recruiting/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class RecruitingConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'recruiting'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "recruiting"
