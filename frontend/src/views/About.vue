9ebcfab112c91fb61fa9c92974d0563a9fd29c18
Hussain Nagaria <hussainbhaitech@gmail.com>
2022-02-06 18:32:11 +0530

    chore: Add some info in about us page

--- frontend/src/views/About.vue
+++ frontend/src/views/About.vue
@@ -1,3 +1,25 @@
 <template>
-	<h2>About us goes here...</h2>
+	<div class="prose prose-indigo">
+		<h1>About Us</h1>
+
+		<p>
+			This is our software project for the course
+			<strong>Agile Software Projects</strong>.
+		</p>
+
+		<p>
+			Hosted at a free plan by
+			<a href="https://render.com">render.com</a>.
+		</p>
+
+		<h2>Our Awesome Team</h2>
+
+		<ul>
+			<li>Alex</li>
+			<li>David</li>
+			<li>Erik</li>
+			<li>Hussain</li>
+			<li>Koki</li>
+		</ul>
+	</div>
 </template>
