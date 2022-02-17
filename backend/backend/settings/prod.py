aa27b747631d6b2d6707128d5aa6ec8e18e33541
Hussain Nagaria <hussainbhaitech@gmail.com>
2022-02-17 15:06:58 +0530

    refactor: Separate prod settings
    * Also, postgres for prod!

---
+++ backend/backend/settings/prod.py
@@ -0,0 +1,14 @@
+from .defaults import *
+
+DEBUG = False
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": os.getenv("DB_PORT"),
+    },
+}
